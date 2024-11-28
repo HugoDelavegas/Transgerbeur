f2955b25f1c5e97008b75b4a4db99376d35225af
Erwan Leroux <erwan.leroux2096@gmail.com>
2024-11-28 09:29:03 +0100

    Commit#1 Création depot Git+Cout

---
+++ RGB/rgb.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "RGB";
+	return(0);
+}
---
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -0,0 +1,10 @@
+//TP5 sruct Enum
+//Leroux Erwan
+//29/11/2024
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Transgerbeur";
+	return(0);
+}
